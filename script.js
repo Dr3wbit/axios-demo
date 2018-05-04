5cb8ca54c13ee49ba529eabb68ac64d2cf3c5ffa
Dr3wbit <eightbitandrew@gmail.com>
2018-05-04 14:38:28 -0700

    axios demo complete

--- script.js
+++ script.js
@@ -1,24 +1,37 @@
 const BASE_URL = 'http://api.reactprototypes.com';
-const API_KEY = '?key=testuser1234';
+const API_KEY = '?key=c118demouser';
 
-axios.get(`${BASE_URL}/todos${API_KEY}`).then(resp => {
-    const { todos } = resp.data;
-    const table = $('table tbody');
+axios.get(`${BASE_URL}/todos${API_KEY}`).then(response => {
+    console.log(response);
 
-    addToDom(todos, table);
+    const listElements = response.data.todos.map((item, index) => {
+        const tr = document.createElement('tr');
+        const number = document.createElement('td');
+        const title = document.createElement('td');
+        const completed = document.createElement('td');
+
+        number.innerText = index + 1;
+        title.innerText = item.title;
+        completed.innerText = item.complete;
+
+        tr.appendChild(number);
+        tr.appendChild(title);
+        tr.appendChild(completed);
+
+        document.querySelector('.table tbody').appendChild(tr);
+    });
 });
 
-const addToDom = (list, container) => {
-    const tableRows = list.map((item, index) => {
-
-        const tableData = [
-            $(`<td>${index + 1}</td>`),
-            $(`<td>${item.title}</td>`),
-            item.complete ? $(`<td class="text-success">Yes</td>`) : $(`<td class="text-danger">No</td>`)
-        ];
-
-        return $('<tr>').append(tableData);
-    });
-
-    container.append(tableRows);
+const todoItem = {
+    tiddle: 'Andrew\'s Super Secret Secrets',
+    details: 'this is a test secret'
 };
+
+axios.post(`${BASE_URL}/todos${API_KEY}`, todoItem).then(response => {
+    console.log(response)
+}).catch(err => {
+    console.log(err.response.data.error);
+
+    console.log(err.message)
+})
+
